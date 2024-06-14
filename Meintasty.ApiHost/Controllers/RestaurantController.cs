9113502b8339388ed5bcc905e0d2697d766f1ad5
keremcann <ce.cankerem@gmail.com>
2024-06-14 22:58:14 +0300

    edited

--- Meintasty.ApiHost/Controllers/RestaurantController.cs
+++ Meintasty.ApiHost/Controllers/RestaurantController.cs
@@ -1,5 +1,7 @@
 ﻿using MediatR;
-using Microsoft.AspNetCore.Http;
+using Meintasty.Application.Contract.Restaurant.Queries;
+using Meintasty.Core.Common;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Meintasty.ApiHost.Controllers
@@ -8,8 +10,28 @@ namespace Meintasty.ApiHost.Controllers
     [ApiController]
     public class RestaurantController : ControllerBase
     {
+        /// <summary>
+        /// 
+        /// </summary>
         private readonly IMediator _mediator;
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="mediator"></param>
         public RestaurantController(IMediator mediator) => _mediator = mediator;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        [Authorize(Roles = "Admin,Member")]
+        [HttpPost("getRestaurantsByCityId")]
+        public async Task<IActionResult> GetRestaurantsByCityId([FromBody] GetRestaurantsByCityIdQueryRequest request)
+        {
+            GeneralResponse<List<GetRestaurantsByCityIdQueryResponse>> response = await _mediator.Send(request);
+            return Ok(response);
+        }
     }
 }
